76e477361ad667952e2642151d7f6d843a586197
jagadish123 <mohantyjagadish123@gmail.com>
2021-05-04 13:03:49 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
+import React,{ useState} from 'react';
 
-function App() {
+const App =()=> {
+  const [name,setName] =useState({
+    value:"",
+    listContent:[]
+  });
+  const onChangeHandler=(event)=>{
+    setName({
+      ...name,
+      value:event.target.value
+    })
+  }
+  const showList= () =>{
+    console.log(name.value);
+    const value=name.value;
+    const list=[...name.listContent];
+    list.push({value:value,id:new Date().getTime()});
+    setName({ 
+      ...name,
+      listContent:list
+    })
+  }
+
+  const deleteItemHandler = (id)=>{
+    const newList=name.listContent.filter(elem=>elem.id!==id)
+    setName({
+      ...name,
+      listContent:newList
+    });
+  }
+
+  const listItems=( 
+    name.listContent.map(elem=>{
+      return (
+        <div key={elem.id} style={{display:'flex',justifyContent:'space-around',alignItems:'center'}}>
+
+        <li >{elem.value}</li>
+        <button onClick={()=>deleteItemHandler(elem.id)}>*</button>
+        </div>
+    )
+    })
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+        <div className={classes.toDoListContent}>
+            <div className={classes.contentHeading}><h2>To Do List</h2></div>
+            <div className={classes.contentControl}>
+              <input type="text" value={name.value} onChange={onChangeHandler} placeholder="add your content"/>
+              
+              <button onClick={showList}>+</button>
+            </div>
+            <ol className={classes.contentList}>
+              {listItems}
+            </ol>
+        </div>
     </div>
   );
 }
